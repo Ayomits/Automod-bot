cc8c115369b5f1b9270e6162b74e2b74e92c15a1
ayo <a2yomidiscord@gmail.com>
2025-07-18 11:47:01 +0300

    fix: test cases

--- backend/test/__init__.py
+++ backend/test/__init__.py
@@ -1,6 +1,1 @@
-from .test_capslock_alghoritm import test_full_caps, test_mixed_caps
-
-__all__ = (
-    "test_full_caps",
-    "test_mixed_caps"
-)
+__all__ = ()
--- backend/test/test_capslock_alghoritm.py
+++ backend/test/test_capslock_alghoritm.py
@@ -8,11 +8,11 @@ def caps_lock_instance():
 def test_full_caps(caps_lock_instance):
     assert caps_lock_instance.analyze("ПРИВЕТ ВСЕМ РАБОТЯЩИМ") is True
 
-def test_full_caps(caps_lock_instance):
+def test_non_full_caps(caps_lock_instance):
     assert caps_lock_instance.analyze("Привет всем работящим") is False
 
 def test_mixed_caps(caps_lock_instance):
     assert caps_lock_instance.analyze("ПрИвЕт вСеМ рАБотящим", type=automod.CapsLockType.Mixed.value) is True
 
-def test_mixed_caps(caps_lock_instance):
+def test_non_mixed_caps(caps_lock_instance):
     assert caps_lock_instance.analyze("ПрИвЕт всем работящим", type=automod.CapsLockType.Mixed.value) is False
