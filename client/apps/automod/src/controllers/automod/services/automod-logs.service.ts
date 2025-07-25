b8107ee98dc20d6563c8b45b64537bfd84daca29
ayo <a2yomidiscord@gmail.com>
2025-07-25 18:05:03 +0300

    feat: bulk logs

--- client/apps/automod/src/controllers/automod/services/automod-logs.service.ts
+++ client/apps/automod/src/controllers/automod/services/automod-logs.service.ts
@@ -4,38 +4,91 @@ import type {
   AutomodRule,
   ObjectKeys,
 } from "@automod/types";
-import type { Guild, Message, SendableChannels, User } from "discord.js";
+import type {
+  APIEmbed,
+  Guild,
+  GuildChannel,
+  Message,
+  SendableChannels,
+  Snowflake,
+  User,
+} from "discord.js";
 import { EmbedBuilder } from "discord.js";
-import { injectable } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 
+import { LocalCache } from "#cache/local.cache.js";
 import { TempConfig } from "#const/temp-config.js";
+import { ScheduleManager } from "#lib/schedule/schedule-manager.js";
 
 import { AutomodLogMessages } from "../../../messages/automod.messages.js";
 
+const EMBEDS_LIMIT = 25;
+const BATCHED_LOG_DELAY = 2_000;
+const SINGLE_LOG_DELAY = 500;
+
 @injectable()
 export class AutomodLogService {
+  constructor(
+    @inject(ScheduleManager) private logTimeoutCache: ScheduleManager,
+    @inject(LocalCache)
+    private logEmbedsCache: LocalCache<Snowflake, APIEmbed[]>
+  ) {}
+
   public async sendLogs(response: AutomodResponse, msg: Message) {
-    const guild = msg.guild!;
+    if (msg.guild) {
+      return;
+    }
     await Promise.allSettled([
-      this.sendDmLog(response, guild, msg.author),
-      this.sendGuildLog(response, guild),
+      this.sendDmLog(response, msg.guild!, msg.author),
+      this.sendGuildLog(response, msg.channel as GuildChannel),
     ]);
   }
 
-  public async sendGuildLog(response: AutomodResponse, guild: Guild) {
-    const resolvedMatches = this.resolveMatches(guild, response.matches);
-    const channel = await this.fetchLogChannel(guild);
-    resolvedMatches.forEach((resolved, idx) => {
-      setTimeout(() => {
-        try {
-          channel.send({
-            embeds: resolved,
-          });
-        } catch {
-          return;
-        }
-      }, 1_000 * idx);
+  public async sendGuildLog(response: AutomodResponse, channel: GuildChannel) {
+    const resolvedMatches = this.resolveMatches(
+      channel.guild,
+      response.matches
+    );
+    const existedTimeout = this.logTimeoutCache.get(channel.id);
+    const existedEmbeds = this.logEmbedsCache.get(channel.id) ?? [];
+    this.logEmbedsCache.set(
+      channel.id,
+      [...existedEmbeds, ...resolvedMatches.map((em) => em.toJSON())],
+      Infinity
+    );
+    if (existedTimeout) {
+      return;
+    }
+    this.logTimeoutCache.set(channel.id, {
+      callback: async () => this.sendScheduledLogs(channel),
+      delay: BATCHED_LOG_DELAY,
     });
+  }
+
+  private async sendScheduledLogs(channel: GuildChannel) {
+    const logChannel = await this.fetchLogChannel(channel.guild);
+    if (!logChannel) {
+      console.log("no log channel");
+      return;
+    }
+
+    const rawEmbeds = this.logEmbedsCache.get(channel.id);
+    this.logEmbedsCache.delete(channel.id);
+
+    if (rawEmbeds.length === 0) {
+      console.log("empty raw");
+      return;
+    }
+
+    for (let i = 0; i < rawEmbeds.length; i += EMBEDS_LIMIT) {
+      const batch = rawEmbeds.slice(i, i + EMBEDS_LIMIT);
+      try {
+        await new Promise((resolve) => setTimeout(resolve, SINGLE_LOG_DELAY));
+        await logChannel.send({ embeds: batch });
+      } catch (error) {
+        console.error("Failed to send log batch:", error);
+      }
+    }
   }
 
   public async sendDmLog(response: AutomodResponse, guild: Guild, user: User) {
@@ -44,7 +97,7 @@ export class AutomodLogService {
       setTimeout(() => {
         try {
           // TODO: разделить отчёты
-          user.send({ embeds: resolved });
+          user.send({ embeds: [resolved] });
         } catch {
           return;
         }
@@ -75,7 +128,7 @@ export class AutomodLogService {
           });
       });
     });
-    return embeds;
+    return embeds.flatMap((em) => em);
   }
 
   private async fetchLogChannel(guild: Guild) {
@@ -86,13 +139,13 @@ export class AutomodLogService {
       })
       .catch(console.error);
     if (!channel) {
-      throw new Error("Log channel does not exists");
+      console.error("Log channel does not exists");
     }
     return channel as SendableChannels;
   }
 
   private getLogMessagesByType(
-    type: ObjectKeys<(typeof AutomodLogMessages)["logging"]>,
+    type: ObjectKeys<(typeof AutomodLogMessages)["logging"]>
   ) {
     return AutomodLogMessages.logging?.[type];
   }
